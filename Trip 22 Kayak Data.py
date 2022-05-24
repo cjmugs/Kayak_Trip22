fac16aca5f9c513e02653161a53b9b81cefe6a57
Chris <cjmugs@gmail.com>
2022-05-24 10:22:10 -0400

    First Commit

---
+++ Trip 22 Kayak Data.py
@@ -0,0 +1,34 @@
+# Data from GPS Garmin #
+Elevation = 420
+Elapse_Time = 6.29
+Stopped_Time = 42.38
+Trip_Odometer = 20.22
+Max_Speed = 6.4
+Moving_Average = 3.1
+Overall_Average = 2.8 + 2.5
+
+# Trip Info #
+Launch_Site = "Boat Dock Road, Florence, IL"
+End_Site = "Port of Grafton, Grafton, IL"
+
+# Stops #
+# 1) Campsite = Right acsending bank (Thursday Night / Friday Morning)
+# 2) Kampsville = The Pavillion (To avoid the rain and get dry) (Friday Afternoon) - Robbie Left due to injury
+# 3) Campsite = Big Fire to dry everything out (Friday Night / Saturday Morning)
+# 4) Campsite = (Saturday Night / Sunday Morning)
+# 5) Emergency Landing = Matt almost drowned due to shock from cut (Sunday Day)
+# 6) Landed at Final Destination = Port of Grafton, Grafton, IL (Sunday Evening)
+
+# Finished out the Illinois River this year #
+
+# Crew #
+def Crew():
+    Captian_Member = {'Name':'Chris', "Years": 11}
+    Captian2_Member = {'Name':'Matt', "Years": 11}  
+    Crew_Member = {'Name':'Steve', "Years": 8}
+    Crew2_Member = {'Name':'Scott', "Years": 8}
+    Crew3_Member = {'Name':'Ben', "Years": 5}
+    Crew4_Member = {'Name':'Robbie', "Years": 5}
+
+# Show Program
+afs
